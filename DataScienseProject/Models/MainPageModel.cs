b47fee8f6656bf0f18b8e255356947c7e54338a5
DESKTOP-6KBSV6E\marij <alyonashipulya@gmail.com>
2021-11-19 11:16:49 +0200

    add MainPageModel

--- DataScienseProject/Models/MainPageModel.cs
+++ DataScienseProject/Models/MainPageModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataScienseProject.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace DataScienseProject.Models
 {
     public class MainPageModel
     {
+        public View Views { get; set; }
+        public ViewExecutor ViewExecutors { get; set; }
+        public ViewTag ViewTags { get; set; }
+        public Element Elements { get; set; }
     }
 }
