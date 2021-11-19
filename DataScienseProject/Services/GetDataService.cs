3355192ff7e87a9c17fd970f482594551c90b434
DESKTOP-6KBSV6E\marij <alyonashipulya@gmail.com>
2021-11-19 13:54:42 +0200

    add reading procedure

--- DataScienseProject/Services/GetDataService.cs
+++ DataScienseProject/Services/GetDataService.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System.Data;
 
 namespace DataScienseProject.Services
 {
@@ -22,6 +23,52 @@ namespace DataScienseProject.Services
         {
             var sqlCommand = new SqlCommand("GetViewData") { CommandType = System.Data.CommandType.StoredProcedure };
             var test = sqlCommand.Parameters.AddWithValue("@ViewKey", 1);
+            
+
+
+
+            List<MainPageModel> resultList = new List<MainPageModel>();
+            IDataReader reader = null;
+            SqlConnection dbConnection = null;
+            //
+            try
+            {
+                dbConnection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB; database=CS_DS_Portfolio; Integrated Security=True;");
+                IDbCommand dbCommand = new SqlCommand();
+                dbCommand.Connection = dbConnection;
+                dbCommand.CommandType = CommandType.StoredProcedure;
+                dbCommand.CommandText = "GetViewData";
+                //
+                dbConnection.Open();
+                //
+                reader = dbCommand.ExecuteReader();
+                //
+                while (reader.Read())
+                {
+                    Object erpEntity = new Object();
+                    erpEntity = reader.GetValue(1);                    
+                    //
+                    //resultList.Add(erpEntity);
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new Exception(exception.Message, exception); //This is good to have!
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+                //
+                dbConnection.Close();
+            }
+            //
+            resultList.ToArray();
+
+
+
+
+
             return new MainPageModel();
         }
         public List<GaleryModel> GetGaleryPageData()
