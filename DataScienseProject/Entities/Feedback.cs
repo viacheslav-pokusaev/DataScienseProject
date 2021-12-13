657bd397f9d3274aec30500f6684b58308bc51e1
Даниил Калинин <42569208+kdaniilm@users.noreply.github.com>
2021-12-13 14:50:21 +0200

    update entities

--- DataScienseProject/Entities/Feedback.cs
+++ DataScienseProject/Entities/Feedback.cs
@@ -9,9 +9,11 @@ namespace DataScienseProject.Entities
     {
         public long FeedbackKey { get; set; }
         public long? VisitKey { get; set; }
+        public long? ViewKey { get; set; }
         public string Email { get; set; }
         public string Text { get; set; }
 
+        public virtual View ViewKeyNavigation { get; set; }
         public virtual VisitLog VisitKeyNavigation { get; set; }
     }
 }
