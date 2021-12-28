2b54feafc785872b7cca52b8bb8db2fca215ef70
Даниил Калинин <42569208+kdaniilm@users.noreply.github.com>
2021-12-28 15:57:36 +0200

    Fix comments

--- DataScienseProject/Interfaces/IEmailSenderService.cs
+++ DataScienseProject/Interfaces/IEmailSenderService.cs
@@ -6,7 +6,5 @@ namespace DataScienseProject.Interfaces
     public interface IEmailSenderService
     {
         public Task SendEmail(EmailSendModel email);
-
-        public Task<string> ConfigureEmail(EmailSendModel email);
     }
 }
--- DataScienseProject/Services/EmailSenderService.cs
+++ DataScienseProject/Services/EmailSenderService.cs
@@ -3,6 +3,8 @@ using DataScienseProject.Interfaces;
 using DataScienseProject.Models;
 using DataScienseProject.Models.EmailSender;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,18 +16,21 @@ namespace DataScienseProject.Services
     public class EmailSenderService : IEmailSenderService
     {
         private readonly DataScienceProjectDbContext _context;
-        public EmailSenderService(DataScienceProjectDbContext context)
+        private readonly IConfiguration _configuration;
+        public EmailSenderService(DataScienceProjectDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task SendEmail(EmailSendModel email)
         {
             var recipients = new List<ConfigModel>();
+            var smtpData = _configuration.GetSection("SmtpData");
 
             recipients = _context.ConfigValues.Where(x => x.Key == "AdminEmail").Select(cv => new ConfigModel { Value = cv.Value, IsEnabled = cv.IsEnabled }).AsNoTracking().ToList();
 
-            MailAddress from = new MailAddress("datasciencecs2021@gmail.com", "Portfolio");
+            MailAddress from = new MailAddress(smtpData.GetSection("senderEmail").Value, smtpData.GetSection("senderName").Value);
 
             MailMessage message = new MailMessage();
 
@@ -41,25 +46,15 @@ namespace DataScienseProject.Services
 
             message.Subject = "Expiration password inserted";
 
-            message.Body = await ConfigureEmail(email).ConfigureAwait(false);
+            message.Body = $"<div><h1>Try to see group: {email.GroupName}</h1><h3>Attempt time: {email.EnterTime}</h3><h3>With password: {email.Password}</h3></div>";
             message.IsBodyHtml = true;
 
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            SmtpClient smtp = new SmtpClient(smtpData.GetSection("smtpHost").Value, Convert.ToInt32(smtpData.GetSection("port").Value));
 
-            smtp.Credentials = new NetworkCredential("datasciencecs2021@gmail.com", "DataScience2021");
+            smtp.Credentials = new NetworkCredential(smtpData.GetSection("senderEmail").Value, smtpData.GetSection("senderEmailPassword").Value);
             smtp.EnableSsl = true;
-            smtp.Send(message);
+            await smtp.SendMailAsync(message);
         }
 
-        public async Task<string> ConfigureEmail(EmailSendModel email)
-        {
-            var res = "<div>";
-            res += $"<h1>Try to see group: {email.GroupName}</h1>";
-            res += $"<h3>Attempt time: {email.EnterTime}</h3>";
-            res += $"<h3>With password: {email.Password}</h3>";
-            res += "</div>";
-
-            return await Task.Run(() => res);
-        }
     }
 }
