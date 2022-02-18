fc7774bffa6cf86df5594720cf6acdd00953821c
Даниил Калинин <42569208+kdaniilm@users.noreply.github.com>
2022-02-18 11:06:24 +0200

    fix incorrect password message

--- DataScienseProject/Services/AuthorizationService.cs
+++ DataScienseProject/Services/AuthorizationService.cs
@@ -35,7 +35,7 @@ namespace DataScienseProject.Services
             if (pass == null)
             {
                 res.StatusCode = 403;
-                res.ErrorMessage = "Password incorect";
+                res.ErrorMessage = "Password is incorect";
             }
             else if (pass != null && DateTime.Compare(DateTime.Now.Date, Convert.ToDateTime(pass.ExpirationDate)) > 0 && pass.ExpirationDate != null)
             {
