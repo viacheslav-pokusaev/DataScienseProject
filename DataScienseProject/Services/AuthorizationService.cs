9da1c6b6d3374ae5367d1efcb964d75fb4831587
Даниил Калинин <42569208+kdaniilm@users.noreply.github.com>
2022-02-23 12:28:06 +0200

    Fix grammar mistake

--- DataScienseProject/Services/AuthorizationService.cs
+++ DataScienseProject/Services/AuthorizationService.cs
@@ -35,7 +35,7 @@ namespace DataScienseProject.Services
             if (pass == null)
             {
                 res.StatusCode = 403;
-                res.ErrorMessage = "Invalid password";
+                res.ErrorMessage = "Password is incorrect";
             }
             else if (pass != null && DateTime.Compare(DateTime.Now.Date, Convert.ToDateTime(pass.ExpirationDate)) > 0 && pass.ExpirationDate != null)
             {
