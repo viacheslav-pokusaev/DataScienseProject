c7eaaa843d576c0d0cba6393f8f0c24d09796747
Eugeny Petrov <eugeneepetrov@gmail.com>
2022-03-17 16:11:47 +0200

    test-8

--- datascine-scripts/stop.sh
+++ datascine-scripts/stop.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/dataciense-project-scripts
 sudo chmod +x stop.sh
 sudo chmod +x clear.sh
 sudo chmod +x start.sh
-docker run -d --name data-sciense-project -p 5000:80 eugennn/data-sciense-project
+docker stop data-sciense-project
